8d3e21bc697b30e5e8f6b9a4bc056851da35e701
Suraj Dhamecha <suraj.dhamecha@qlogic.io>
2019-11-28 09:19:14 -0500

    Fix Dependency

--- google-cloud-clients/google-cloud-spanner/src/main/java/com/google/cloud/spanner/TraceUtil.java
+++ google-cloud-clients/google-cloud-spanner/src/main/java/com/google/cloud/spanner/TraceUtil.java
@@ -21,43 +21,17 @@ import com.google.cloud.Timestamp;
 import com.google.common.collect.ImmutableMap;
 import com.google.spanner.v1.Transaction;
 import io.opencensus.contrib.grpc.util.StatusConverter;
-import io.opencensus.implcore.internal.SimpleEventQueue;
-import io.opencensus.implcore.trace.RecordEventsSpanImpl;
-import io.opencensus.implcore.trace.export.ExportComponentImpl;
-import io.opencensus.implcore.trace.export.InProcessSampledSpanStoreImpl;
-import io.opencensus.testing.common.TestClock;
 import io.opencensus.trace.AttributeValue;
 import io.opencensus.trace.EndSpanOptions;
 import io.opencensus.trace.Span;
-import io.opencensus.trace.SpanContext;
-import io.opencensus.trace.SpanId;
 import io.opencensus.trace.Status;
-import io.opencensus.trace.TraceId;
-import io.opencensus.trace.TraceOptions;
-import io.opencensus.trace.Tracestate;
-import io.opencensus.trace.config.TraceParams;
+import io.opencensus.trace.Tracer;
+import io.opencensus.trace.Tracing;
+import io.opencensus.trace.export.SampledSpanStore;
 import java.util.Map;
-import java.util.Random;
 
 /** Utility methods for tracing. */
 class TraceUtil {
-
-  private static final InProcessSampledSpanStoreImpl sampledSpanStore =
-      (InProcessSampledSpanStoreImpl)
-          ExportComponentImpl.createWithInProcessStores(new SimpleEventQueue())
-              .getSampledSpanStore();
-  private static final RecordEventsSpanImpl.StartEndHandler recordEventSpan =
-      new RecordEventsSpanImpl.StartEndHandler() {
-        @Override
-        public void onStart(RecordEventsSpanImpl span) {
-          // Do nothing.
-        }
-
-        @Override
-        public void onEnd(RecordEventsSpanImpl span) {
-          sampledSpanStore.considerForSampling(span);
-        }
-      };
 
   static Map<String, AttributeValue> getTransactionAnnotations(Transaction t) {
     return ImmutableMap.of(
@@ -98,7 +72,7 @@ class TraceUtil {
   }
 
   /**
-   * Appends a list of span names.
+   * Appends the list of span names for sample collection.
    *
    * <p>If called multiple times the library keeps the list of unique span names from all the calls.
    *
@@ -106,35 +80,11 @@ class TraceUtil {
    */
   static void exportSpans(String... spans) {
     Preconditions.checkNotNull(spans);
+    Tracer tracer = Tracing.getTracer();
+    SampledSpanStore sampledSpanStore = Tracing.getExportComponent().getSampledSpanStore();
     for (String spanName : spans) {
-      RecordEventsSpanImpl recordEventsSpan = createSampledSpan(spanName);
-      recordEventsSpan.end(EndSpanOptions.builder().setSampleToLocalSpanStore(true).build());
+      Span span = tracer.spanBuilder(spanName).setRecordEvents(true).startSpan();
+      span.end(EndSpanOptions.builder().setSampleToLocalSpanStore(true).build());
     }
-  }
-
-  /**
-   * Create span to records trace events.
-   *
-   * @param spanName span names.
-   */
-  private static RecordEventsSpanImpl createSampledSpan(String spanName) {
-    Random random = new Random();
-    SpanId parentSpanId = SpanId.generateRandomId(random);
-    SpanContext sampledSpanContext =
-        SpanContext.create(
-            TraceId.generateRandomId(random),
-            SpanId.generateRandomId(random),
-            TraceOptions.builder().setIsSampled(true).build(),
-            Tracestate.builder().build());
-    return RecordEventsSpanImpl.startSpan(
-        sampledSpanContext,
-        spanName,
-        Span.Kind.CLIENT,
-        parentSpanId,
-        false,
-        TraceParams.DEFAULT,
-        recordEventSpan,
-        null,
-        TestClock.create());
   }
 }
